4fb2fcb59cd0cd9b2450eaeec646f938740ca5a0
kiyota takeshi <kiyota@tagbangers.co.jp>
2020-07-13 09:56:09 +0900

    iam settings

--- handler.py
+++ handler.py
@@ -1,10 +1,15 @@
-import json
-import time
+# import json
+# import time
+import boto3
 
 
 def hello(event, context):
     # print("hi !")
     # print("first update")
-    print("second update")
-    time.sleep(4)
-    return "hello-world"
+    # print("second update")
+    # time.sleep(4)
+    # return "hello-world"
+
+    client = boto3.client('lambda')
+    response = client.list_functions()
+    return response
